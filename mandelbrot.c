33bae6b44cd7aedb8f4714cd48804d0283e6ca8a
shkayaexitgit config --global user.name shkayagit config --global user.email shkaya@student.42tokyo.jp <shkaya@student.42tokyo.jp git config --global user.name shkaya>
2024-06-19 13:22:51 +0900

    Mandatory part almost finished

--- mandelbrot.c
+++ mandelbrot.c
@@ -18,7 +18,7 @@ void	put_pixel_to_image(t_data *data, int x, int y, int color)
 // カラーマッピングをする関数
 int	get_color(int iter, int max)
 {
-    if (iter == max)
+	if (iter == max)
     	return (0x00000000);
     /*
 		発散する点(0<t<1)は*色付きで、発散していない点(t = 1)は黒で表現する。
@@ -26,15 +26,15 @@ int	get_color(int iter, int max)
     
         多項式関数(3次や2次)にすることで滑らかな曲線、グラデーションを作る?
     */
-   	else
+	else
     {
 		// 反復回数の割合を0 ~ 255(1byte)で表す。
     	double t = (double)iter / max;
         // (double)でキャストしていない -> intで処理されて0になってた?
     
-    	int	r = (int)(9 * t * t * t * 256); //　赤色成分の計算
-    	int	g = (int)(15 * (1 - t) * t * t * 256); // 緑色成分の計算
-    	int	b = (int)(8.5 * (1 - t) * (1 - t) * t * 256); // 青色成分の計算
+    	int	r = (int)(9 * (1 - t) * t * t * t * 256); //　赤色成分の計算
+    	int	g = (int)(15 * (1 - t) * (1 - t) * t * t * 256); // 緑色成分の計算
+    	int	b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 256); // 青色成分の計算
     	// (1 - t)とtがあることで、どちらかに偏っていた時、必ず0に近くなる。
         // いい感じのグラデーションになるらしい。
     	return (r << 16 | g << 8 | b);
@@ -58,8 +58,8 @@ void	draw_mandelbrot(t_data *data)
             */
             // 画面の各ピクセルを -2.0 ~ 2.0 の範囲の座標に変える。(正規化)
             // x(実部), y(虚部(iの係数))をそれぞれ求める。
-            c_re = data->scale * (x - WIDTH / 2) * (4.0 / WIDTH) + data->offset_x; //実部
-            c_im = data->scale * (y - HEIGHT / 2) * (4.0 / HEIGHT) + data->offset_y;//虚部
+            c_re = data->scale * (x - WIDTH / 2.0) * (4.0 / WIDTH) + data->offset_x; //実部
+            c_im = data->scale * (y - HEIGHT / 2.0) * (4.0 / HEIGHT) + data->offset_y;//虚部
             // 繰り返し計算をし、発散するか確認。
             z_re = c_re;
             z_im = c_im;
@@ -70,8 +70,8 @@ void	draw_mandelbrot(t_data *data)
                 z_im2 = z_im * z_im;
                 if (z_re2 + z_im2 > 4.0)
                 	break; // 発散した時ループ終了。
-                z_re = z_re2 - z_im2 + c_re;
                 z_im = 2 * z_re * z_im + c_im;
+                z_re = z_re2 - z_im2 + c_re;
                 iter++;
             }
             /*
@@ -80,7 +80,7 @@ void	draw_mandelbrot(t_data *data)
             */
         	// 反復回数に基づいて色の深さを決める関数。
             color = get_color(iter, MAX_ITER);
-        	// 実際に色をつけていく関数。
+        	// 画像に色をつけていく関数。
             put_pixel_to_image(data, x, y, color);
         }
     }
